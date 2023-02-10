945ff1606af521705e1ef9f27c2e1ba95d4eb72f
princeLoyal <princeloyal1234@gmail.com>
2023-02-10 20:12:40 +0100

    update

--- frontend/src/pages/EventsPage.js
+++ frontend/src/pages/EventsPage.js
@@ -1,30 +1,38 @@
-import { useLoaderData, json } from 'react-router-dom';
-
+import { useLoaderData, json, defer, Await} from 'react-router-dom';
+import { Suspense } from 'react';
 import EventsList from '../components/EventsList';
 
 function EventsPage() {
-  const data = useLoaderData();
+  const { events } = useLoaderData();
 
-  if(data.isError){
-    return <p>{data.message}</p>
-  }
-  const events = data.events;
+  // if(data.isError){
+  //   return <p>{data.message}</p>
+  // }
+  // const events = data.events;
   
   return (
-    <>
-       <EventsList events={events} />
-    </>
+    <Suspense>
+    <Await resolve={events}>
+    {   (loadedEvent) => <EventsList events={loadedEvent}/> }
+    </Await>
+    </Suspense>
   );
 };
 
 export default EventsPage;
 
-export async function loader (){
-    const response = await fetch('http://localhost:8080/events');
-    if (!response.ok) {
-      return json({message:' Could not fetch data'}), 
-      { status: 500}
-    } else {
-     return response;
-    };
+const loadEvents = async () => {
+  const response = await fetch('http://localhost:8080/events');
+  if (!response.ok) {
+    throw json({message:' Could not fetch events'}), 
+    { status: 500}
+  } else {
+   return response;
+  };
+};
+
+export function loader (){
+  defer({
+    events: loadEvents(),
+  });
  };
